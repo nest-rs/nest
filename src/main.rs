cdce24599ea88aa3ce6352efc68214f4242b93ba
Thomas Harrison <twh2898@vt.edu>
2017-07-18 13:54:47 -0400

    Added html color support, re-implemented draw_rect

--- src/main.rs
+++ src/main.rs
@@ -7,10 +7,12 @@ use std::collections::HashMap;
 #[macro_use]
 mod support;
 
+pub type Color = [f32; 4];
+
 enum Shape {
-    Clear([f32; 4]),
-    Polygon(Vec<(f32, f32)>, [f32; 4]),
-    Line(f32, f32, f32, f32, [f32; 4]),
+    Clear(Color),
+    Polygon(Vec<(f32, f32)>, Color),
+    Line(f32, f32, f32, f32, Color),
 }
 
 pub struct Window {
@@ -141,6 +143,46 @@ impl Window {
         self.color = [red, green, blue, alpha];
     }
 
+    pub fn set_color_html(&mut self, color: &str) {
+        let mut bytes = color.as_bytes();
+        if bytes[0] == b'#' {
+            bytes = &bytes[1..];
+        }
+
+        use std::str::from_utf8;
+        use std::u8;
+
+        if bytes.len() == 3 {
+            let r = u8::from_str_radix(from_utf8(&vec![bytes[0], bytes[0]]).unwrap(), 16).unwrap();
+            let g = u8::from_str_radix(from_utf8(&vec![bytes[1], bytes[1]]).unwrap(), 16).unwrap();
+            let b = u8::from_str_radix(from_utf8(&vec![bytes[2], bytes[2]]).unwrap(), 16).unwrap();
+            self.set_color(
+                (r as f32) / 255.0,
+                (g as f32) / 255.0,
+                (b as f32) / 255.0,
+                1.0,
+            );
+        } else if bytes.len() == 6 {
+            let r = u8::from_str_radix(from_utf8(&bytes[0..2]).unwrap(), 16).unwrap();
+            let g = u8::from_str_radix(from_utf8(&bytes[2..4]).unwrap(), 16).unwrap();
+            let b = u8::from_str_radix(from_utf8(&bytes[4..6]).unwrap(), 16).unwrap();
+            self.set_color(
+                (r as f32) / 255.0,
+                (g as f32) / 255.0,
+                (b as f32) / 255.0,
+                1.0,
+            );
+        }
+    }
+
+    pub fn draw_rect(&mut self, p1: (f32, f32), p2: (f32, f32)) {
+        let shape = Shape::Polygon(
+            vec![(p1.0, p1.1), (p1.0, p2.1), (p2.0, p2.1), (p2.0, p1.1)],
+            self.color,
+        );
+        self.shapes.push(shape);
+    }
+
     pub fn draw_line(&mut self, x1: f32, y1: f32, x2: f32, y2: f32) {
         let shape = Shape::Line(x1, y1, x2, y2, self.color);
         self.shapes.push(shape);
@@ -158,8 +200,11 @@ fn main() {
     window.start_loop(|app, delta| {
         app.clear_color(0.1, 0.3, 0.2, 1.0);
 
-        app.set_color(0.3, 0.1, 0.2, 1.0);
-        app.draw(&[(0.0, 0.0), (10.0, 10.0)]);
+        app.set_color_html("312");
+        app.draw_rect((-0.5, -0.5), (0.5, 0.5));
+
+        app.set_color_html("#033112");
+        app.draw_rect((0.0, 0.0), (-1.0, -1.0));
 
         app.set_color(1.0, 0.0, 0.0, 1.0);
         app.draw_line(0.0, 0.0, 1.0, 1.0);
