8f1544693933467ad2a19007c70e7f3e3e59b751
Thomas Harrison <twh2898@vt.edu>
2017-07-19 02:47:47 -0400

    Added keymap to store keystates

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,11 @@ extern crate love2d;
 use love2d::{Window, Action, Event};
 use love2d::{ElementState, VirtualKeyCode};
 
+use std::collections::HashMap;
+
 fn main() {
     let mut window = Window::new("Hello World", 640, 480);
+    let mut keymap: HashMap<VirtualKeyCode, bool> = HashMap::new();
 
     window.start_loop(|app, _| {
         app.clear();
@@ -30,8 +33,16 @@ fn main() {
         for ev in app.poll_events() {
             match ev {
                 Event::Closed => return Action::Stop,
-                Event::KeyboardInput(ElementState::Pressed, Some(VirtualKeyCode::Space)) => {
-                    println!("Space!");
+                Event::KeyboardInput(ElementState::Pressed, Some(key)) => {
+                    match key {
+                        VirtualKeyCode::Space => println!("Space!"),
+                        VirtualKeyCode::Escape => return Action::Stop,
+                        _ => (),
+                    };
+                    keymap.insert(key, true);
+                }
+                Event::KeyboardInput(ElementState::Released, Some(key)) => {
+                    keymap.insert(key, false);
                 }
                 _ => (),
             }
