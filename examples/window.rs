7d37e61fc4c58b171b3258724b64f71ebb46ff84
Thomas Harrison <twh2898@vt.edu>
2017-07-21 13:55:49 -0400

    Added simple window example

---
+++ examples/window.rs
@@ -0,0 +1,23 @@
+
+extern crate love2d;
+use love2d::{Window, Event};
+
+fn main() {
+	let mut app = Window::new("Window Example", 640, 480);
+
+	'main: loop {
+		{
+			let mut frame = app.next_frame();
+			frame.clear();
+
+			frame.finish();
+		}
+
+		for ev in app.poll_events() {
+			match ev {
+				Event::Closed => break 'main,
+				_ => (),
+			}
+		}
+	}
+}
