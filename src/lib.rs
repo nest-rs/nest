46da5796146acaa7bc7533661548bbcc567bf334
Thomas Harrison <twh2898@vt.edu>
2017-07-29 03:16:38 -0400

    Added Shape transform code

--- src/lib.rs
+++ src/lib.rs
@@ -62,7 +62,6 @@ extern crate glium;
 extern crate image as img;
 
 pub mod support;
-mod events;
 mod frame;
 mod window;
 mod image;
@@ -71,7 +70,7 @@ mod shapes;
 pub use window::Window;
 pub use frame::Frame;
 pub use shapes::*;
-pub use events::Event;
+pub use support::events::Event;
 pub use image::Image;
 pub use image::LoadImageError;
 pub use image::ImageParameters;
