0a49a073ad5c0ded3b3c13ba5bcbd1c5bf32da76
Geordon Worley <vadixidav@gmail.com>
2017-09-26 03:05:41 -0400

    add more comments to demo

--- examples/demo.rs
+++ examples/demo.rs
@@ -11,25 +11,29 @@ fn main() {
 
     // Load the petal texture.
     let petal_texture = app.load_image("examples/petal.png").unwrap();
-    // Create an image rectangle from the petal texture with a width of 0.15 and proportional height.
-    let petal = image_w(petal_texture, 0.4);
+    // Create an image rectangle from the petal texture with a width of 0.4 and proportional height.
+    let petal = image_w(petal_texture, 0.4).translate([0.3, 0.0]);
 
-    let flower = (0usize..6).flat_map(|i| petal.translate([0.3, 0.0])
-                                                       .rotate(i as f32 / 6.0 * 2.0 * PI)
-                                             ).collect::<Vec<_>>();
+    // Create flower from 6 petals rotated around the center.
+    let flower = (0usize..6)
+        .flat_map(|i| petal.rotate(i as f32 / 6.0 * 2.0 * PI))
+        .collect::<Vec<_>>();
 
     loop {
         // Handle events.
         for event in app.poll_events() {
             match event {
                 // Close if they close the window or hit escape.
-                Event::Closed | Event::KeyboardInput(KeyState::Pressed, Some(Key::Escape)) => return,
+                Event::Closed | Event::KeyboardInput(KeyState::Pressed, Some(Key::Escape)) => {
+                    return
+                }
                 // Print "Space!" if they hit space.
                 Event::KeyboardInput(KeyState::Pressed, Some(Key::Space)) => println!("Space!"),
                 _ => {}
             }
         }
 
+        // Draw the flower rotating at 1 rad/sec.
         app.draw(flower.rotate(start.elapsed().to_secs()));
     }
 }
